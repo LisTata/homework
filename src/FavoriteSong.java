f5b510e71f44832f83a8775540263db0a2fe516e
LisTata <lisitsa13ht@gmail.com>
2022-09-29 11:01:46 +0300

    FavoriteSong

---
+++ src/FavoriteSong.java
@@ -0,0 +1,6 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        //Manafest-Firestarter
+        System.out.println("I wanna be a firestarter\nYou can help me if you wanna\nWe can spread and make the whole world light up\nLight up,light up.");
+    }
+}
